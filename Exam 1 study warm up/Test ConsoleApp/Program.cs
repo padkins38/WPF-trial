08e9d33f3a52d0a7f19032fd484d79823da3c4df
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-10-11 20:06:11 -0500

    Api URL concatination

---
+++ Exam 1 study warm up/Test ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Test_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string urlFront = "https://rickandmortyapi.com/api/character?page=";
+            double page = 1;
+
+
+            string url2 = string.Concat(urlFront, page);
+
+            while (page < 35)
+            {
+                string url = ($"{urlFront}{page.ToString()}");
+                Console.WriteLine($"{url} | {url2}");
+
+
+                page++;
+            }
+        }       
+    }
+}
---
+++ WpfApp1/(P) Rick And Morty- AddCharToCboBox/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace _P__Rick_And_Morty__AddCharToCboBox
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cboCharacters.Items.Clear();
+
+            using (var client = new HttpClient())
+            {
+                string urlFront = "https://rickandmortyapi.com/api/character?page=";
+                double page = 1;
+
+                while (page < 35) //need to update
+                {
+                    string url = string.Concat(urlFront, page.ToString());
+                    string jsonData = client.GetStringAsync(url).Result;
+
+
+                    RickAndMortyAPI api = JsonConvert.DeserializeObject<RickAndMortyAPI>(jsonData);
+
+                    foreach (Character item in api.results)
+                    {
+                        cboCharacters.Items.Add(item);
+                    }
+
+                    page++;
+                }
+            }
+           
+            //CharactersList = https://rickandmortyapi.com/api/character
+        }
+    }
+}
